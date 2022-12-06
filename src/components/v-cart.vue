bf169507b3cc21569de7dae1bfc783bceeda85e2
Novik_Maria <novik.work.mail@gmail.com>
2022-12-06 11:28:01 +0300

    learning vue - lesson 4

---
+++ src/components/v-cart.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="v-cart"></div>
+</template>
+
+<script>
+export default {
+  name: "v-cart.vue",
+  props:{},
+  data(){
+    return{}
+  },
+  computed:{}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="v-main-wrapper">
+    <v-catalog />
+    <v-cart />
+  </div>
+</template>
+<script>
+import vCatalog from './v-catalog';
+import vCart from './v-cart'
+export default {
+  name: 'v-main-wrapper',
+  components:{
+    vCatalog,
+    vCart
+  },
+  props:{},
+  data(){
+    return{
+
+    }
+  },
+  computed:{},
+  methods:{},
+  watch: {},
+  mounted() {
+
+  }
+}
+</script>
+<style>
+.v-main-wrapper {
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
